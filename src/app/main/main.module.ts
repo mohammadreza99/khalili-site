7865f39aa057c747886cab2e5ddeb17fc164c996
solmaz.shokri <solmaz.shokri.motlagh.73@gmail.com>
2020-08-29 08:32:09 -0700

    ..

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,11 +2,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/cor
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home/home.page';
 import { SharedModule } from '@app/shared/shared.module';
-
+import { NgxImageZoomModule } from 'ngx-image-zoom';
 @NgModule({
   declarations: [HomePage],
   exports: [HomePage],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule,NgxImageZoomModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
 })
 export class MainModule {}

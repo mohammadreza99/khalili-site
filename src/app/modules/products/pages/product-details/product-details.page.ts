2fc7a2b553385993b97394ff40f7391532149f38
mohammadreza shahbazi <king.mohammadreza@yahoo.com>
2020-08-30 19:46:02 +0430

    ..

--- src/app/modules/products/pages/product-details/product-details.page.ts
+++ src/app/modules/products/pages/product-details/product-details.page.ts
@@ -3,13 +3,68 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'product-details',
   templateUrl: './product-details.page.html',
-  styleUrls: ['./product-details.page.scss']
+  styleUrls: ['./product-details.page.scss'],
 })
 export class ProductDetailsPage implements OnInit {
+  products = [
+    {
+      img: '../../../assets/images/1.jpg',
+      title:
+        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
+      price: '34499000',
+      discount: '50',
+      link: '',
+    },
+    {
+      img: '../../../assets/images/2.jpg',
+      title:
+        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
+      price: '34499000',
+      discount: '50',
+      link: '',
+    },
+    {
+      img: '../../../assets/images/3.jpg',
+      title:
+        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
+      price: '34499000',
+      discount: '50',
+      link: '',
+    },
+    {
+      img: '../../../assets/images/4.jpg',
+      title:
+        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
+      price: '34499000',
+      discount: '50',
+      link: '',
+    },
+    {
+      img: '../../../assets/images/5.jpg',
+      title:
+        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
+      price: '34499000',
+      discount: '50',
+      link: '',
+    },
+    {
+      img: '../../../assets/images/6.jpg',
+      title:
+        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
+      price: '34499000',
+      discount: '50',
+      link: '',
+    },
+    {
+      img: '../../../assets/images/7.jpg',
+      title:
+        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
+      price: '34499000',
+      discount: '50',
+      link: '',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

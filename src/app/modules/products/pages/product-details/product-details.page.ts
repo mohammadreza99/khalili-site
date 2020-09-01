69076b963abafb4999fc69cf85456d0ee7d1da0a
mohammad reza <king.mohammadreza@yahoo.com>
2020-09-01 23:14:20 +0430

    .

--- src/app/modules/products/pages/product-details/product-details.page.ts
+++ src/app/modules/products/pages/product-details/product-details.page.ts
@@ -6,64 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.page.scss'],
 })
 export class ProductDetailsPage implements OnInit {
-  products = [
-    {
-      img: '../../../assets/images/1.jpg',
-      title:
-        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
-      price: '34499000',
-      discount: '50',
-      link: '',
-    },
-    {
-      img: '../../../assets/images/2.jpg',
-      title:
-        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
-      price: '34499000',
-      discount: '50',
-      link: '',
-    },
-    {
-      img: '../../../assets/images/3.jpg',
-      title:
-        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
-      price: '34499000',
-      discount: '50',
-      link: '',
-    },
-    {
-      img: '../../../assets/images/4.jpg',
-      title:
-        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
-      price: '34499000',
-      discount: '50',
-      link: '',
-    },
-    {
-      img: '../../../assets/images/5.jpg',
-      title:
-        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
-      price: '34499000',
-      discount: '50',
-      link: '',
-    },
-    {
-      img: '../../../assets/images/6.jpg',
-      title:
-        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
-      price: '34499000',
-      discount: '50',
-      link: '',
-    },
-    {
-      img: '../../../assets/images/7.jpg',
-      title:
-        'گوشی موبایل اپل مدل iPhone 11 Pro A2217 دو سیم‌ کارت ظرفیت 256 گیگابایت',
-      price: '34499000',
-      discount: '50',
-      link: '',
-    },
-  ];
+ 
   constructor() {}
 
   ngOnInit(): void {}

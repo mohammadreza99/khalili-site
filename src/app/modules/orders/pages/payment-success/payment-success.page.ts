bbfb7d8f95067466c0016ba307ef0e78d38c16f0
mohammadreza shahbazi <king.mohammadreza@yahoo.com>
2020-10-08 23:47:14 +0330

    ..

--- src/app/modules/orders/pages/payment-success/payment-success.page.ts
+++ src/app/modules/orders/pages/payment-success/payment-success.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'payment-success',
   templateUrl: './payment-success.page.html',
-  styleUrls: ['./payment-success.page.scss']
+  styleUrls: ['./payment-success.page.scss'],
 })
 export class PaymentSuccessPage implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
-
+  trackingNumber = "123";
   ngOnInit(): void {
+    this.trackingNumber = this.route.snapshot.queryParamMap.get(
+      'trackingNumber'
+    );
   }
-
 }
